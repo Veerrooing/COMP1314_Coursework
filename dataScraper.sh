47df81b8d20e990c663eb56918e4d32a34d650de
Ong Jie Son <jso1u24@soton.ac.uk>
2025-11-27 01:15:50 +0800

    Added data scraping function

---
+++ dataScraper.sh
@@ -0,0 +1,18 @@
+HTML=$(curl -s "https://coinmarketcap.com/currencies/bitcoin/" -o "temp.html")
+
+if [ $? -ne 0 ]; then
+    echo "Error: Opening Page."
+    exit 1
+fi
+
+priceCurrent=$(grep -oE 'data-test="text-cdp-price-display">\$[0-9,]+(\.[0-9]+)?' "temp.html" | sed -E 's/.*data-test="text-cdp-price-display">//; s/[$,]//g')
+
+priceLow=$(grep -oE 'eQBACe label">Low</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">Low</div><span>##; s/[$,]//g')
+
+priceHigh=$(grep -oE 'eQBACe label">High</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">High</div><span>##; s/[$,]//g')
+
+
+echo $priceCurrent
+echo $priceHigh
+echo $priceLow
+
