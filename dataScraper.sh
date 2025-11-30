54655b3dc8fd3d1fb7f294867557dbf3dae1cf2e
Ong Jie Son <jso1u24@gmail.com>
2025-11-30 12:46:26 +0800

    Added tracker for gold, silver and etheruem

--- dataScraper.sh
+++ dataScraper.sh
@@ -5,17 +5,57 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-priceCurrent=$(grep -oE 'data-test="text-cdp-price-display">\$[0-9,]+(\.[0-9]+)?' "temp.html" | sed -E 's/.*data-test="text-cdp-price-display">//; s/[$,]//g')
+BTCpriceCurrent=$(grep -oE 'data-test="text-cdp-price-display">\$[0-9,]+(\.[0-9]+)?' "temp.html" | sed -E 's/.*data-test="text-cdp-price-display">//; s/[$,]//g')
 
-priceLow=$(grep -oE 'eQBACe label">Low</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">Low</div><span>##; s/[$,]//g')
+BTCpriceLow=$(grep -oE 'eQBACe label">Low</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">Low</div><span>##; s/[$,]//g')
 
-priceHigh=$(grep -oE 'eQBACe label">High</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">High</div><span>##; s/[$,]//g')
+BTCpriceHigh=$(grep -oE 'eQBACe label">High</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">High</div><span>##; s/[$,]//g')
 
-
-echo $priceCurrent
-echo $priceHigh
-echo $priceLow
-
-"/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO BTC (currentPrice, highPrice, lowPrice) VALUES ($priceCurrent, $priceHigh, $priceLow);"
+"/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO BTC (BTCcurrentPrice, BTChighPrice, BTClowPrice) VALUES ($BTCpriceCurrent, $BTCpriceHigh, $BTCpriceLow);"
 
 rm temp.html
+
+HTML=$(curl -s "https://www.kitco.com/charts/gold" -o "temp.html")
+
+if [ $? -ne 0 ]; then
+    echo "Error: Opening Page."
+    exit 1
+fi
+
+GOLDpriceCurrent=$(grep -oE '<p class="CommodityPrice_priceName__Ehicd capitalize">ounce</p><p class="CommodityPrice_convertPrice__5Addh ml-auto justify-self-end">[0-9,]+(\.[0-9]+)?' "temp.html" | sed -E 's/.*<p class="CommodityPrice_priceName__Ehicd capitalize">ounce<\/p><p class="CommodityPrice_convertPrice__5Addh ml-auto justify-self-end">//; s/,//g')
+
+"/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO GOLD (AUcurrentPrice) VALUES ($GOLDpriceCurrent);"
+
+rm temp.html
+
+HTML=$(curl -s "https://www.kitco.com/charts/silver" -o "temp.html")
+
+if [ $? -ne 0 ]; then
+    echo "Error: Opening Page."
+    exit 1
+fi
+
+SILVERpriceCurrent=$(grep -oE '<p class="CommodityPrice_priceName__Ehicd capitalize">ounce</p><p class="CommodityPrice_convertPrice__5Addh ml-auto justify-self-end">[0-9,]+(\.[0-9]+)?' "temp.html" | sed -E 's/.*<p class="CommodityPrice_priceName__Ehicd capitalize">ounce<\/p><p class="CommodityPrice_convertPrice__5Addh ml-auto justify-self-end">//; s/,//g')
+
+"/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO SILVER (AGcurrentPrice) VALUES ($SILVERpriceCurrent);"
+
+rm temp.html
+
+HTML=$(curl -s "https://coinmarketcap.com/currencies/ethereum/" -o "temp.html")
+
+if [ $? -ne 0 ]; then
+    echo "Error: Opening Page."
+    exit 1
+fi
+
+ETHpriceCurrent=$(grep -oE 'data-test="text-cdp-price-display">\$[0-9,]+(\.[0-9]+)?' "temp.html" | sed -E 's/.*data-test="text-cdp-price-display">//; s/[$,]//g')
+
+ETHpriceLow=$(grep -oE 'eQBACe label">Low</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">Low</div><span>##; s/[$,]//g')
+
+ETHpriceHigh=$(grep -oE 'eQBACe label">High</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">High</div><span>##; s/[$,]//g')
+
+"/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO ETH (ETHcurrentPrice, ETHhighPrice, ETHlowPrice) VALUES ($ETHpriceCurrent, $ETHpriceHigh, $ETHpriceLow);"
+
+rm temp.html
+
+echo "Done"
