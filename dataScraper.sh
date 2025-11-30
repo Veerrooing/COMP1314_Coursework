1899a91467af71c655c857cded4a9187add3939e
Ong Jie Son <jso1u24@gmail.com>
2025-11-30 14:42:49 +0800

    Added logs to the script

--- dataScraper.sh
+++ dataScraper.sh
@@ -1,3 +1,5 @@
+timestamp=$(date + "%Y-%m-%d_%H-%M-%S")
+
 HTML=$(curl -s "https://coinmarketcap.com/currencies/bitcoin/" -o "temp.html")
 
 if [ $? -ne 0 ]; then
@@ -13,6 +15,8 @@ BTCpriceHigh=$(grep -oE 'eQBACe label">High</div><span>\$[0-9,]+(\.[0-9]+)?' tem
 
 "/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO BTC (BTCcurrentPrice, BTChighPrice, BTClowPrice) VALUES ($BTCpriceCurrent, $BTCpriceHigh, $BTCpriceLow);"
 
+echo "$timestamp : BTC Current Price : $BTCpriceCurrent, BTC Low Price : $BTCpriceLow, BTC High Price : $BTCpriceHigh"
+
 rm temp.html
 
 HTML=$(curl -s "https://www.kitco.com/charts/gold" -o "temp.html")
@@ -26,6 +30,8 @@ GOLDpriceCurrent=$(grep -oE '<p class="CommodityPrice_priceName__Ehicd capitaliz
 
 "/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO GOLD (AUcurrentPrice) VALUES ($GOLDpriceCurrent);"
 
+echo "$timestamp : Gold Current Price : $GOLDpriceCurrent"
+
 rm temp.html
 
 HTML=$(curl -s "https://www.kitco.com/charts/silver" -o "temp.html")
@@ -38,6 +44,8 @@ fi
 SILVERpriceCurrent=$(grep -oE '<p class="CommodityPrice_priceName__Ehicd capitalize">ounce</p><p class="CommodityPrice_convertPrice__5Addh ml-auto justify-self-end">[0-9,]+(\.[0-9]+)?' "temp.html" | sed -E 's/.*<p class="CommodityPrice_priceName__Ehicd capitalize">ounce<\/p><p class="CommodityPrice_convertPrice__5Addh ml-auto justify-self-end">//; s/,//g')
 
 "/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO SILVER (AGcurrentPrice) VALUES ($SILVERpriceCurrent);"
+
+echo "$timestamp : Silver Current Price : $SILVERpriceCurrent"
 
 rm temp.html
 
@@ -56,6 +64,8 @@ ETHpriceHigh=$(grep -oE 'eQBACe label">High</div><span>\$[0-9,]+(\.[0-9]+)?' tem
 
 "/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO ETH (ETHcurrentPrice, ETHhighPrice, ETHlowPrice) VALUES ($ETHpriceCurrent, $ETHpriceHigh, $ETHpriceLow);"
 
+echo "$timestamp : ETH Current Price : $ETHpriceCurrent, ETH Low Price : $ETHpriceLow, ETH High Price : $ETHpriceHigh"
+
 rm temp.html
 
-echo "Done"
+echo "$timestamp : Data inserted successfully"
