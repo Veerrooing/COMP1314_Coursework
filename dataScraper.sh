0f966c7a9562ed522e6f4728cb7e84699d8a547d
Ong Jie Son <jso1u24@soton.ac.uk>
2025-12-02 14:36:15 +0800

    Change it to be compatible with WSL

--- dataScraper.sh
+++ dataScraper.sh
@@ -5,6 +5,7 @@ HTML=$(curl -s "https://coinmarketcap.com/currencies/bitcoin/" -o "temp.html")
 
 if [ $? -ne 0 ]; then
     echo "Error: Opening Page."
+    echo "Stopping Script"
     exit 1
 fi
 
@@ -14,7 +15,7 @@ BTCpriceLow=$(grep -oE 'eQBACe label">Low</div><span>\$[0-9,]+(\.[0-9]+)?' temp.
 
 BTCpriceHigh=$(grep -oE 'eQBACe label">High</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">High</div><span>##; s/[$,]//g')
 
-"/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO BTC (BTCcurrentPrice, BTChighPrice, BTClowPrice) VALUES ($BTCpriceCurrent, $BTCpriceHigh, $BTCpriceLow);"
+"/mnt/c/xampp/mysql/bin/mysql.exe" -u root -e "USE Crypto_DB; INSERT INTO BTC (BTCcurrentPrice, BTChighPrice, BTClowPrice) VALUES ($BTCpriceCurrent, $BTCpriceHigh, $BTCpriceLow);"
 
 echo "$timestamp : BTC Current Price : $BTCpriceCurrent, BTC Low Price : $BTCpriceLow, BTC High Price : $BTCpriceHigh"
 
@@ -29,7 +30,7 @@ fi
 
 GOLDpriceCurrent=$(grep -oE '<p class="CommodityPrice_priceName__Ehicd capitalize">ounce</p><p class="CommodityPrice_convertPrice__5Addh ml-auto justify-self-end">[0-9,]+(\.[0-9]+)?' "temp.html" | sed -E 's/.*<p class="CommodityPrice_priceName__Ehicd capitalize">ounce<\/p><p class="CommodityPrice_convertPrice__5Addh ml-auto justify-self-end">//; s/,//g')
 
-"/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO GOLD (AUcurrentPrice) VALUES ($GOLDpriceCurrent);"
+"/mnt/c/xampp/mysql/bin/mysql.exe" -u root -e "USE Crypto_DB; INSERT INTO GOLD (AUcurrentPrice) VALUES ($GOLDpriceCurrent);"
 
 echo "$timestamp : Gold Current Price : $GOLDpriceCurrent"
 
@@ -44,7 +45,7 @@ fi
 
 SILVERpriceCurrent=$(grep -oE '<p class="CommodityPrice_priceName__Ehicd capitalize">ounce</p><p class="CommodityPrice_convertPrice__5Addh ml-auto justify-self-end">[0-9,]+(\.[0-9]+)?' "temp.html" | sed -E 's/.*<p class="CommodityPrice_priceName__Ehicd capitalize">ounce<\/p><p class="CommodityPrice_convertPrice__5Addh ml-auto justify-self-end">//; s/,//g')
 
-"/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO SILVER (AGcurrentPrice) VALUES ($SILVERpriceCurrent);"
+"/mnt/c/xampp/mysql/bin/mysql.exe" -u root -e "USE Crypto_DB; INSERT INTO SILVER (AGcurrentPrice) VALUES ($SILVERpriceCurrent);"
 
 echo "$timestamp : Silver Current Price : $SILVERpriceCurrent"
 
@@ -63,7 +64,7 @@ ETHpriceLow=$(grep -oE 'eQBACe label">Low</div><span>\$[0-9,]+(\.[0-9]+)?' temp.
 
 ETHpriceHigh=$(grep -oE 'eQBACe label">High</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">High</div><span>##; s/[$,]//g')
 
-"/opt/lampp/bin/mysql" -u root -e "USE Crypto_DB; INSERT INTO ETH (ETHcurrentPrice, ETHhighPrice, ETHlowPrice) VALUES ($ETHpriceCurrent, $ETHpriceHigh, $ETHpriceLow);"
+"/mnt/c/xampp/mysql/bin/mysql.exe" -u root -e "USE Crypto_DB; INSERT INTO ETH (ETHcurrentPrice, ETHhighPrice, ETHlowPrice) VALUES ($ETHpriceCurrent, $ETHpriceHigh, $ETHpriceLow);"
 
 echo "$timestamp : ETH Current Price : $ETHpriceCurrent, ETH Low Price : $ETHpriceLow, ETH High Price : $ETHpriceHigh"
 
